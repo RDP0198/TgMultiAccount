f1d298281e9fdc6cbb651ea13c829e84f5c42604
Ankush <r8552580882@gmail.com>
2026-03-02 19:14:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 
 __version__ = '1.0.1'
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres:CRP5E5CgEgPNTDl2@db.cjvslvnriptthwpnqlmp.supabase.co:5432/postgres')
